171249d91009cef036f04998cbd2fec2fb56e3f1
abenezer147 <abenezerdaniel147@gmail.com>
2026-09-22 08:47:30 +0300

    Script: Anchor point top left

---
+++ README.md
@@ -0,0 +1,3 @@
+# Useful After Effects Scripts
+
+A collection of useful After Effects scripts to boost up productivity.
---
+++ anchor_point_top_left.jsx
@@ -0,0 +1,10 @@
+﻿var currentComp = app.project.activeItem;
+var selectedLayers = currentComp.selectedLayers;
+
+for(var i = 0; i < selectedLayers.length; i++) {
+    var selectedLayer = selectedLayers[i];
+    var sourceRect = selectedLayer.sourceRectAtTime(0, false);
+    var anchorPoint = selectedLayer.anchorPoint;
+    
+    anchorPoint.setValue([sourceRect.left, sourceRect.top]);
+}
